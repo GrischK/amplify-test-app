5fb2dbebad299801fec6b25a3927cf79ae0e7a6b
GrischK <grischka.gorski@laposte.net>
2024-10-11 10:57:05 +0200

    ⚡️ IsDone default falsy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import type {Schema} from "../amplify/data/resource";
 import {generateClient} from "aws-amplify/data";
+import {cli} from "aws-cdk/lib";
 
 const client = generateClient<Schema>();
 
@@ -14,7 +15,7 @@ function App() {
     }, []);
 
     function createTodo() {
-        client.models.Todo.create({content: window.prompt("Todo content"), isDone:false});
+        client.models.Todo.create({content: window.prompt("Todo content"), isDone: false});
     }
 
     function deleteTodo(id: string) {
@@ -24,8 +25,12 @@ function App() {
     function updateTodo(id: string, currentContent: string) {
         const updatedContent = window.prompt("Update Todo content", currentContent);
         if (updatedContent) {
-            client.models.Todo.update({ id, content: updatedContent });
+            client.models.Todo.update({id, content: updatedContent});
         }
+    }
+
+    function updateTodoIsDone(id: string, currentIsDone: boolean) {
+        client.models.Todo.update({id, isDone: !currentIsDone})
     }
 
     return (
@@ -36,11 +41,13 @@ function App() {
                 {todos.map((todo) => (
                     <li key={todo.id}
                         style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>{todo.content}
-                        <div style={{display: 'flex', alignItems: 'center', gap:'12px'}}>
+                        <div style={{display: 'flex', alignItems: 'center', gap: '12px'}}>
                             <button onClick={() => deleteTodo(todo.id)}>Delete</button>
                             <button onClick={() => updateTodo(todo.id, todo.content || '')}>Update</button>
+                            <input type="checkbox" checked={todo.isDone ? todo.isDone : false}
+                                   onChange={() => updateTodoIsDone(todo.id, todo.isDone)}
+                            />
                         </div>
-
                     </li>
                 ))}
             </ul>
