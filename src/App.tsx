6b0791e6b50503222234ee13782e8d6701770938
GrischK <grischka.gorski@laposte.net>
2024-10-11 17:03:25 +0200

    🚧 Trying to get Tags in Todo object

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,56 @@ const client = generateClient<Schema>();
 
 function App() {
     const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const [tags, setTags] = useState<Array<Schema["Tag"]["type"]>>([]);
+    const [displayForm, setDisplayForm] = useState<boolean>(false);
+
+    console.log("todos ", todos)
+    console.log("tags ", tags)
 
     useEffect(() => {
         // A chaque changement dans les données on met à jour les todos
-        const sub = client.models.Todo.observeQuery().subscribe({
+        const todoSub = client.models.Todo.observeQuery().subscribe({
             next: (data) => setTodos([...data.items]),
         });
 
+        const tagSub = client.models.Tag.observeQuery().subscribe({
+            next: (data) => setTags([...data.items]),
+        });
+
         // On se désabonne de l'observation lorsque le composant est démonté pour éviter les effets de bord
-        return () => sub.unsubscribe();
+        return () => {
+            todoSub.unsubscribe();
+            tagSub.unsubscribe()
+        };
+
     }, []);
 
-    function createTodo() {
-        client.models.Todo.create({content: window.prompt("Todo content"), isDone: false});
+    function createTodo(formData: FormData) {
+        const todoData = {
+            content: formData.get('content')?.toString() || '',
+            isDone: false
+        };
+
+        client.models.Todo.create(todoData).then((createdTodo) => {
+            const todoId = createdTodo.data?.id;
+
+            const selectedTags = formData.getAll('tags') as Array<string>;
+
+            selectedTags.forEach((tagId) => {
+                client.models.TodoTag.create({
+                    todoId: todoId!,
+                    tagId: tagId
+                });
+            });
+        });
+    }
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        createTodo(formData);
+        setDisplayForm(false);
     }
 
     function deleteTodo(id: string) {
@@ -39,7 +76,7 @@ function App() {
     return (
         <main>
             <h1>My todos</h1>
-            <button onClick={createTodo}>+ new</button>
+            <button onClick={() => setDisplayForm(true)}>+ new</button>
             <ul>
                 {todos.map((todo) => (
                     <li key={todo.id}
@@ -49,11 +86,20 @@ function App() {
                                    onChange={() => updateTodoIsDone(todo.id, !!todo.isDone)}
                             />
                             <span>{todo.content}</span>
+                            {/*<span>{todo.tags.map((tag)=>tag.name)}</span>*/}
                         </div>
                         <div style={{display: 'flex', alignItems: 'center', gap: '12px'}}>
                             <button onClick={() => deleteTodo(todo.id)}>Delete</button>
                             <button onClick={() => updateTodo(todo.id, todo.content || '')}>Update</button>
                         </div>
+                    </li>
+                ))}
+            </ul>
+            <h2>My tags</h2>
+            <ul>
+                {tags.map((tag) => (
+                    <li key={tag.id}>
+                        {tag.name}
                     </li>
                 ))}
             </ul>
@@ -64,6 +110,84 @@ function App() {
                     Review next step of this tutorial.
                 </a>
             </div>
+            {
+                displayForm && (
+                    <div style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        zIndex: 10,
+                        width: "100wv",
+                        height: "100vh",
+                        backgroundColor: 'rgba(0, 0, 0, 0.6)',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                        <div style={{
+                            backgroundColor: 'white',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            flexDirection: 'column',
+                            padding: '12px',
+                            borderRadius: '10px',
+                            position: 'relative',
+                        }}>
+                            <button style={{
+                                color: 'white',
+                                borderRadius: '10px',
+                                width: '20px',
+                                height: '20px',
+                                padding: '0',
+                                position: "absolute",
+                                top: '5px',
+                                right: '5px',
+                                fontSize:'12px'
+                            }}
+                                    onClick={() => setDisplayForm(false)}>X
+                            </button>
+                            <h3>Create todo</h3>
+                            <form onSubmit={handleSubmit} style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                flexDirection: 'column',
+                                gap: '12px'
+                            }}>
+                                <div>
+                                    <label htmlFor="content">
+                                        <input type="text" id="content" name="content" placeholder={'Enter todo content'}
+                                               required/>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label htmlFor="tags" style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        flexDirection: 'column',
+                                        gap: '12px'
+                                    }}>
+                                        Select tag
+                                        <select name="tags" id="tags" multiple>
+                                            {tags.map((tag) => (
+                                                <option key={tag.id} value={tag.id}>
+                                                    {tag.name}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </label>
+                                </div>
+                                <button type={"submit"}>Create</button>
+                            </form>
+                        </div>
+
+                    </div>
+                )
+            }
         </main>
     );
 }
