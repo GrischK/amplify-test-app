a35d5d392fb038e250f3c3673174f7594e0e055e
GrischK <grischka.gorski@laposte.net>
2024-10-15 14:42:58 +0200

    ⏪️ Rollback

--- src/App.tsx
+++ src/App.tsx
@@ -128,18 +128,7 @@ function App() {
 
 
     return (
-        <Authenticator
-            signUpAttributes={['email', 'preferred_username']}
-            formFields={{
-                signUp: {
-                    preferred_username: {
-                        label: 'Username', // Remplace "Preferred Username" par "Username"
-                        placeholder: 'Enter your username', // Personnalise le placeholder
-                        isRequired: true,
-                    }
-                }
-            }}
-        >
+        <Authenticator>
             {({signOut, user}) => {
                 console.log(user);
                 return(
